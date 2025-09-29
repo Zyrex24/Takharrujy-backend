c62ea38cd4928927585cc65403e73a226d70282a
bakry56 <abdobakry932@gmail.com>
2025-09-29 14:54:53 +0300

    Deleted ChatMessageRepository

--- src/main/java/com/university/takharrujy/domain/repository/ChatMessageRepository.java
+++
@@ -1,9 +0,0 @@
-package com.university.takharrujy.domain.repository;
-
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> {
-    Page<ChatMessage> findByProjectIdOrderByTimestampDesc(Long projectId, Pageable pageable);
-}
